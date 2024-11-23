7b2c781047e1f518563a4919ecb6fa17958aa512
HiLFi12 <martinleandrorota@gmail.com>
2024-11-23 19:02:37 -0300

    UnitTest 1
    Position

--- ProyectoBase - 02-09/UnitTestProject1/UnitTest1.cs
+++ ProyectoBase - 02-09/UnitTestProject1/UnitTest1.cs
@@ -8,41 +8,42 @@ namespace UnitTestProject1
     public class UnitTest1
     {
 
-        [TestMethod]
-        public void TestRenderComp()
-        {
-            TransformData SpawnTest = new TransformData();
-            SpawnTest.SetPosition(500, 500);
-            string path = "GameAssets/movimiento1.png";
-            // Entity test = new Entity("pepe", 10, 10, 10, SpawnTest, "GameAssets/movimiento1.png");
-            Entity test1;
-            test1 = new Entity(SpawnTest, "GameAssets/movimiento1.png");
-            string texturaOK = test1.texture;
-            Assert.AreEqual(path, texturaOK);
-            Console.Write("\n");
-            Console.Write(test1.texture);
+        //[TestMethod]
+        //public void TestRenderComp()
+        //{
+        //    TransformData SpawnTest = new TransformData();
+        //    SpawnTest.SetPosition(500, 500);
+        //    string path = "GameAssets/movimiento1.png";
+        //    // Entity test = new Entity("pepe", 10, 10, 10, SpawnTest, "GameAssets/movimiento1.png");
+        //    Entity test1;
+        //    test1 = new Entity(SpawnTest, "GameAssets/movimiento1.png");
+        //    string texturaOK = test1.texture;
+        //    Assert.AreEqual(path, texturaOK);
+        //    Console.Write("\n");
+        //    Console.Write(test1.texture);
            
 
             
+        //}
+
+
+
+        [TestMethod]
+        //PRUEBA DE POSICION, METODO SETPOSITION
+        public void TestTransform()
+        {
+            TransformData transform = new TransformData();
+            float x, y;
+            x = 5;
+            y = 10;
+
+            transform.SetPosition(x, y);
+
+            Assert.AreEqual(x, transform.PositionX);
+            Console.Write(transform);
+            Console.Write("\n");
+            Console.Write(transform.PositionX);
         }
 
-
-
-        //[TestMethod]
-        //public void TestTransform()
-        //{
-        //    TransformData transform = new TransformData() ;
-        //    float x, y;
-        //    x = 5;
-        //    y = 10;
-
-        //    transform.SetPosition(x, y);
-
-        //    Assert.AreEqual(x, transform.PositionX);
-        //    Console.Write(transform);
-        //    Console.Write("\n");
-        //    Console.Write("ok");
-        //}
-      
     }
 }
